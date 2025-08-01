50407583e7cc4f2b0d855b0adcd4d249ee96f3b1
gominsky <gominsky@gmail.com>
2025-08-01 09:08:14 +0200

    db.js con init.js

--- database/db.js
+++ database/db.js
@@ -1,12 +1,11 @@
-// db.js (nuevo para PostgreSQL)
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // necesario en Render
+    rejectUnauthorized: false
   }
 });
-require('./init');
+
 module.exports = pool;
+
