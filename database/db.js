8b87038d6a1f12b11f39a0fef80db48f6c0d8f59
gominsky <gominsky@gmail.com>
2025-08-12 23:21:00 +0200

    Pre-entrevista

--- database/db.js
+++ database/db.js
@@ -1,11 +1,15 @@
-const { Pool } = require('pg');
+// db.js (nuevo para PostgreSQL)
+const { Pool, types } = require('pg');
+
+// Forzar que los campos NUMERIC (OID 1700) se devuelvan como float
+types.setTypeParser(1700, val => parseFloat(val));
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  user: 'appuser',
+  host: 'localhost',
+  database: 'appdb',
+  password: 'apppass',
+  port: 5432,
 });
 
 module.exports = pool;
-
